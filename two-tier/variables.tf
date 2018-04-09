88d57232f868ec2c9621eaa6dfef63a8a59a0df9
Wade <wddhao@foxmail.com>
2018-04-09 09:59:00 +0800

    AWS

---
+++ two-tier/variables.tf
@@ -0,0 +1,27 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-west-2"
+}
+
+# Ubuntu Precise 12.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    cn-north-1b = "ami-d8578bb5"
+    cn-north-1b = "ami-d8578bb5"
+    cn-north-1b = "ami-d8578bb5"
+  }
+}
